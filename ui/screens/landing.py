e87f2cd3c52e9c2f3fefb8f2c54490016cc27712
Hunny <unreal5_gonza@outlook.es>
2024-04-26 22:28:01 -0300

    Acomoda el landing.

--- ui/screens/landing.py
+++ ui/screens/landing.py
@@ -27,51 +27,52 @@ def draw(frame, change_screen):
     row_index = 0
     row_offset = 50
     for key, value in user_progress.items():
-        column_offset = 40
-        capitalized_module_name = [word.capitalize() for word in key.split('_')]
-        output_capitalized_module_name = " ".join(capitalized_module_name)
-        canvas.create_text(150, 100 + row_offset, text=f"{row_index}. {output_capitalized_module_name}", fill="#e8e8e3", font=("Georgia", 15, "bold"), anchor="nw")
+        if row_index < 5:
+            column_offset = 40
+            #capitalized_module_name = [word.capitalize() for word in key.split('_')]
+            #output_capitalized_module_name = " ".join(capitalized_module_name)
+            #canvas.create_text(150, 100 + row_offset, text=f"{row_index}. {output_capitalized_module_name}", fill="#e8e8e3", font=("Georgia", 15, "bold"), anchor="nw")
 
-        if value["status"] != "LOCKED":
-            image_book = Image.open(resource_path("assets\\images\\books\\"+str(row_index+1)+".png"))
-            image_book = image_book.resize((40, 50))
-            image_book_tk = ImageTk.PhotoImage(image_book)
-            setattr(canvas, f"image_book_{row_index}", image_book_tk)
-            canvas.create_image(400, 90 + row_offset, anchor=tk.NW, image=image_book_tk)
+            if value["status"] != "LOCKED":
+                image_book = Image.open(resource_path("assets\\images\\books\\"+str(row_index+1)+".png"))
+                image_book = image_book.resize((40, 50))
+                image_book_tk = ImageTk.PhotoImage(image_book)
+                setattr(canvas, f"image_book_{row_index}", image_book_tk)
+                canvas.create_image(600, 80 + row_offset, anchor=tk.NW, image=image_book_tk)
 
-        for i in range(value["total"]):
-            state = "LOCKED" if value["status"] == "LOCKED" else "IN_PROGRESS" if i == value["current"] else "LOCKED" if i > value["current"] else "COMPLETED"
-            screen_to_change = Screens[f'{key}_{i}'.upper()]
-            levels_image_path = None
-            if state == "IN_PROGRESS":
-                levels_image_path = resource_path("assets\\images\\levels\\"+str(row_index)+"-current.png")
-            elif state == "LOCKED":
-                levels_image_path = resource_path("assets\\images\\levels\\locked.png")
-            elif state == "COMPLETED":
-                levels_image_path = resource_path("assets\\images\\levels\\"+str(row_index)+"-passed.png")
+            for i in range(value["total"]):
+                state = "LOCKED" if value["status"] == "LOCKED" else "IN_PROGRESS" if i == value["current"] else "LOCKED" if i > value["current"] else "COMPLETED"
+                screen_to_change = Screens[f'{key}_{i}'.upper()]
+                levels_image_path = None
+                if state == "IN_PROGRESS":
+                    levels_image_path = resource_path("assets\\images\\levels\\"+str(row_index)+"-current.png")
+                elif state == "LOCKED":
+                    levels_image_path = resource_path("assets\\images\\levels\\locked.png")
+                elif state == "COMPLETED":
+                    levels_image_path = resource_path("assets\\images\\levels\\"+str(row_index)+"-passed.png")
 
-            image_level = Image.open(levels_image_path)
-            image_level = image_level.resize((50, 90))
-            image_level_tk = ImageTk.PhotoImage(image_level)
+                image_level = Image.open(levels_image_path)
+                image_level = image_level.resize((50, 90))
+                image_level_tk = ImageTk.PhotoImage(image_level)
 
-            def on_image_click(event, screen=screen_to_change):
-                change_screen(screen)
-                canvas.destroy()
+                def on_image_click(event, screen=screen_to_change):
+                    change_screen(screen)
+                    canvas.destroy()
 
-            def on_image_enter(event):
-                canvas.config(cursor="hand2")
+                def on_image_enter(event):
+                    canvas.config(cursor="hand2")
 
-            def on_image_leave(event):
-                canvas.config(cursor="")
+                def on_image_leave(event):
+                    canvas.config(cursor="")
 
-            setattr(canvas, f"image_level_tk_{row_index}_{i}", image_level_tk)
-            button = canvas.create_image(430 + column_offset, 80 + row_offset, anchor=tk.NW, image=image_level_tk)
-            if state != "LOCKED":
-                canvas.tag_bind(button, "<Enter>", on_image_enter)
-                canvas.tag_bind(button, "<Leave>", on_image_leave)
-                canvas.tag_bind(button, '<Button-1>', on_image_click)
+                setattr(canvas, f"image_level_tk_{row_index}_{i}", image_level_tk)
+                button = canvas.create_image(640 + column_offset, 70 + row_offset, anchor=tk.NW, image=image_level_tk)
+                if state != "LOCKED":
+                    canvas.tag_bind(button, "<Enter>", on_image_enter)
+                    canvas.tag_bind(button, "<Leave>", on_image_leave)
+                    canvas.tag_bind(button, '<Button-1>', on_image_click)
 
-            column_offset += 60
+                column_offset += 60
 
-        row_offset += 80
+        row_offset += 95
         row_index += 1
